e296bbde1e7d4f39da7dcd2e4236035a6a2c37f1
Innovatiff <mateuszgredes@gmail.com>
2026-02-16 04:57:24 +0100

    Zmiana jezyka na polski

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,17 +7,27 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			defaultLocale: 'pl', // Ustawiamy polski jako domyślny
+			locales: {
+				pl: {
+					label: 'Polski',
+					lang: 'pl',
+				},
+			},
+			social: [
+				{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }
+			],
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'Poradniki', // Zamiast Guides
 					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						// Pamiętaj, żeby potem zmienić też folder/plik na dysku, jeśli chcesz zachować porządek
+						// Ale na razie slug 'guides/example' może zostać, to tylko adres URL
+						{ label: 'Przykładowy Poradnik', slug: 'guides/example' },
 					],
 				},
 				{
-					label: 'Reference',
+					label: 'Baza Wiedzy', // Zamiast Reference
 					autogenerate: { directory: 'reference' },
 				},
 			],
