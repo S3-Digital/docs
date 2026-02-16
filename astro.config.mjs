361787a258bde7222805aee03b52fb9175e6fa09
Innovatiff <mateuszgredes@gmail.com>
2026-02-16 17:16:52 +0100

    zapisuje biezace zmiany przed pull

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,11 +18,20 @@ export default defineConfig({
 				{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }
 			],
 			sidebar: [
+<<<<<<< HEAD
     {
         label: 'Dokumentacja',
         autogenerate: { directory: 'docs' } // lub po prostu directory: '.' zależnie gdzie masz pliki
     }
 ]
+=======
+				{
+					label: 'Przewodniki',
+					// Opcja 1: Szukaj wszystkiego w folderze docs
+					autogenerate: { directory: '.' },
+				},
+			],
+>>>>>>> c9856ea (zapisuje biezace zmiany przed pull)
 		}),
 	],
 });
